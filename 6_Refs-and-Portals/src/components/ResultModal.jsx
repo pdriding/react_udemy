e9e1805c71cd6a36ff93cc51bdd2a236ddaaa1a1
pdriding <peterdriding@gmail.com>
2025-03-21 14:44:58 +0400

    Management App first commit

--- 6_Refs-and-Portals/src/components/ResultModal.jsx
+++ 6_Refs-and-Portals/src/components/ResultModal.jsx
@@ -1,7 +1,19 @@
 import { useRef, useImperativeHandle } from "react";
+import { createPortal } from "react-dom";
 
-export default function ResultModal({ ref, result, targetTime }) {
+export default function ResultModal({
+  ref,
+  targetTime,
+  remainingTime,
+  onReset,
+}) {
   const dialog = useRef();
+
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
+
+  console.log(targetTime);
 
   useImperativeHandle(ref, () => {
     return {
@@ -11,18 +23,21 @@ export default function ResultModal({ ref, result, targetTime }) {
     };
   });
 
-  return (
-    <dialog ref={dialog} className="result-modal">
-      <h2>You {result}</h2>
+  return createPortal(
+    <dialog ref={dialog} className="result-modal" onClose={onReset}>
+      {userLost && <h2>You lost</h2>}
+      {!userLost && <h2>Your Score: {score}</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left.</strong>
+        You stopped the timer with{" "}
+        <strong>{formattedRemainingTime} seconds left.</strong>
       </p>
-      <form method="dialog">
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById("modal")
   );
 }
