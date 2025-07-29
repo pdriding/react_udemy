75fa5c4b2a22e017d9ec8ec41febb7ca0616224c
pdriding <48731550+pdriding@users.noreply.github.com>
2025-07-29 11:56:07 +0400

    finished routing practice

---
+++ 17_Routing-SPA/src/components/MainNavigation.js
@@ -0,0 +1,33 @@
+import { NavLink } from "react-router-dom";
+
+import classes from "./MainNavigation.module.css";
+
+function MainNavigation() {
+  return (
+    <header className={classes.header}>
+      <nav>
+        <ul className={classes.list}>
+          <li>
+            <NavLink
+              className={({ isActive }) => (isActive ? classes.active : "")}
+              to="/"
+              end
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={({ isActive }) => (isActive ? classes.active : "")}
+              to="/products"
+            >
+              Procuts
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default MainNavigation;
---
+++ 17_Routing-SPA/src/pages/Root.js
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import MainNavigation from "../components/MainNavigation";
+
+function RootLayout() {
+  return (
+    <>
+      {" "}
+      <main>
+        <MainNavigation />
+        <Outlet />
+      </main>
+    </>
+  );
+}
+
+export default RootLayout;
