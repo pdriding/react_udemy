cafe3ea48ead7944d6c3e86c7e759f57d681d72c
pdriding <48731550+pdriding@users.noreply.github.com>
2025-07-31 11:33:26 +0400

    Finished Routing App

--- 18_PROJECT_Routing-App/frontend/src/pages/EventDetailsPage.js
+++ 18_PROJECT_Routing-App/frontend/src/pages/EventDetailsPage.js
@@ -1,22 +1,31 @@
 // src/pages/EventDetailsPage.js
-import { useRouteLoaderData, redirect } from "react-router-dom";
+import { useRouteLoaderData, redirect, Await } from "react-router-dom";
 import EventItem from "../components/EventItem";
+import EventsList from "../components/EventsList";
+import { Suspense } from "react";
 
 function EventDetailPage() {
-  const data = useRouteLoaderData("event-detail");
-  console.log(data);
+  const { event, events } = useRouteLoaderData("event-detail");
 
   return (
     <>
-      <EventItem event={data.event} />
+      <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+        <Await resolve={event}>
+          {(loadedEvent) => <EventItem event={loadedEvent} />}
+        </Await>
+      </Suspense>
+      <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+        <Await resolve={events}>
+          {(loadedEvents) => <EventsList events={loadedEvents} />}
+        </Await>
+      </Suspense>
     </>
   );
 }
 
 export default EventDetailPage;
 
-export async function Loader({ params }) {
-  const id = params.eventId;
+async function loadEvent(id) {
   const response = await fetch(`http://localhost:8080/events/${id}`);
 
   if (!response.ok) {
@@ -31,8 +40,30 @@ export async function Loader({ params }) {
     );
   }
 
-  const data = await response.json();
-  return data;
+  const resData = await response.json();
+  return resData.event;
+}
+
+async function loadEvents() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    // return { isError: true, message: "Could not fetch events" };
+    throw new Response(JSON.stringify({ message: "Could not fetch events" }), {
+      status: 500,
+    });
+  } else {
+    const data = await response.json();
+    return data.events || data; // return the actual array
+  }
+}
+
+export async function Loader({ params }) {
+  const id = params.eventId;
+  return {
+    event: await loadEvent(id),
+    events: loadEvents(),
+  };
 }
 
 export async function action({ params, request }) {
