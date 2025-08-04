a1262f85909792705e7dbe2852562b38729ece62
pdriding <48731550+pdriding@users.noreply.github.com>
2025-08-04 12:44:15 +0400

    Beginning Testing

---
+++ 23_Hooks-Context/src/containers/Products.js
@@ -0,0 +1,24 @@
+import ProductItem from "../components/Products/ProductItem";
+import "./Products.css";
+import { ProductsContext } from "../context/products-context";
+import { useContext } from "react";
+
+const Products = (props) => {
+  const productList = useContext(ProductsContext).products;
+
+  return (
+    <ul className="products-list">
+      {productList.map((prod) => (
+        <ProductItem
+          key={prod.id}
+          id={prod.id}
+          title={prod.title}
+          description={prod.description}
+          isFav={prod.isFavorite}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default Products;
